c7419312b64804d1e19f7765fb26d58e81897f34
Niels Olesen <no@tec.dk>
2023-09-08 11:41:59 +0200

    Add project files.

---
+++ DigitalSustainabilityLaboratory/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DigitalSustainabilityLaboratory.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
